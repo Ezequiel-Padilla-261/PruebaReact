4513e45b70cf16c77d91524a4be6b190b854cd60
Ezequiel-Padilla-261 <theeze216@gmail.com>
2019-12-08 18:38:59 -0300

    medidor de scroll e identacion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-$( document ).ready(function() {
-    $('#menu_bar').click(function(){
-    	$('body').toggleClass('visible_menu');
-    })
+$(document).ready(function() {
+  $("#menu_bar").click(function() {
+    $("body").toggleClass("visible_menu");
+  });
+  $(window).scroll(function() {
+    let sc = $(window).scrollTop();
+    if (sc > 500) {
+      $(".header").addClass("header_2");
+      $(".btn-header").addClass("btn-header_2");
+      $(".logo").addClass("logo_2");
+      $(".menu_hamburger").addClass("menu_hamburger_2");
+    } else {
+      $(".header").removeClass("header_2");
+      $(".btn-header").removeClass("btn-header_2");
+      $(".logo").removeClass("logo_2");
+      $(".menu_hamburger").removeClass("menu_hamburger_2");
+    }
+  });
 });
